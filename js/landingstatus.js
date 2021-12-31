e64a402d8f7b203cb8ddf834cd5ed8049f074cde
Francisco Ayala <hatbruh@gmail.com>
2021-12-31 04:55:15 -0500

    new

---
+++ js/landingstatus.js
@@ -0,0 +1,5 @@
+let logName = document.getElementById('loggedIn')
+let logLink = document.getElementById('loggedInLink')
+
+let loginBtn = document.getElementById('loginbtn')
+let signupBtn = document.getElementById('signupbtn')
